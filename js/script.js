138133285cbbf624a0619bf87b15502efa1caa8e
Carlos Garcia <decca2122@gmail.com>
2018-11-15 23:23:13 +0000

    java

--- js/script.js
+++ js/script.js
@@ -5,11 +5,25 @@
 //GLOBAL VARIABLES
 /* global $ */
 $("#shoot").click(function() {
-    alert("here")
     $("#userChoice").text($("#input").val());
 });
-
-
-
 // DOCUMENT READY FUNCTION BELOW
+var userChoice= "";
+var computerChoice= "";
+var winner="";
+var randomNumber= Math.random();
+$("#shoot").click(function() {
+    var userChoice= $("#input").val();
+    $("#userChoice").text(userChoice);
+if (randomNumber <.50 ){
+    computerChoice ="rock";
+}
+else if (randomNumber >.60 && randomNumber < 1.00){
+    computerChoice = "paper";
+}
+else {
+        computerChoice = "scissors"
+}
+$("#computerChoice").text(computerChoice);
+});
 
